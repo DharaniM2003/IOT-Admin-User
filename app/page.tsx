268ae580f83f0186afc1aa1a1123b830fa18f586
DharaniM2003 <mdharani203@gmail.com>
2025-05-27 11:35:31 +0530

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -274,7 +274,7 @@ export default function HomePage() {
           </div>
 
           <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-            <p>&copy; 2024 IoT Hardware Store. All rights reserved.</p>
+            <p>&copy; 2025 IoT Hardware Store. All rights reserved.</p>
           </div>
         </div>
       </footer>
